bbb5648c7ad3396d8080d1e80a805d81a78363ef
fr0staman <77665176+fr0staman@users.noreply.github.com>
2021-05-16 12:58:18 +0300

    broken frontend

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Typerpage from './Typerpage'
 import CreateMatch from './CreateMatch'
 import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
 import { 
     BrowserRouter as Router, 
     Switch, 
@@ -18,13 +19,26 @@ export default class Homepage extends Component{
     render() {
         return (
         <Router>
+            
+            <AppBar>
+            <header class="header">
+                <Button color="blue" to="/">TYPERPRO </Button>
+                    <div class="bar">
+                        <Button color="blue" to="/">HallOfFame </Button>
+                    </div>
+                    <div class="lgn">
+                    <Button color="blue" to="/">Логін </Button>
+                    </div>
+            </header>
+            </AppBar>
             <Switch>
                 <Route exact path='/'>
                     <Button color="primary" variant="contained" align="center" to="/create" component={Link}>
                     НЄ
-                    </Button></Route>
+                    </Button>
+                    </Route>
                 <Route path='/create' component={CreateMatch} />
-                <Route path='/type' component={Typerpage} />
+                <Route path='/type' component={Typerpage} />    
             </Switch>
         </Router>
         );
